6c3f0b6c0d55af66977e148515bda3e63b08ab4d
Balram Pandey <balrampandey19@gmail.com>
2017-12-07 17:33:59 +0530

    get list of sensors aviliable in phone

--- app/src/main/java/troops/com/android_sensors/MainActivity.java
+++ app/src/main/java/troops/com/android_sensors/MainActivity.java
@@ -1,13 +1,32 @@
 package troops.com.android_sensors;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private SensorManager mSensorManager;
+    String sensorText = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSensorManager =
+                (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList =
+                mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor currentSensor : sensorList) {
+            sensorText = sensorText + " " + currentSensor.getName() + System.getProperty("line.separator") + " " + System.getProperty("line.separator");
+        }
+        TextView sensorTextView = (TextView) findViewById(R.id.sensor_list);
+        sensorTextView.setText(sensorText);
+
     }
 }
