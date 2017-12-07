5e27b6aa6e710d8bcc7e4fe56f1cb924b7f54206
Balram Pandey <balrampandey19@gmail.com>
2017-12-07 17:46:52 +0530

    Base Activty

--- app/src/main/java/troops/com/android_sensors/BaseActicity.java
+++ app/src/main/java/troops/com/android_sensors/BaseActicity.java
@@ -1,13 +1,28 @@
 package troops.com.android_sensors;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BaseActicity extends AppCompatActivity {
+    private Activity mActivity;
+    private Button sensorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base_acticity);
+        mActivity = this;
+        sensorList = findViewById(R.id.button_sensorsList);
+        sensorList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mActivity, SensorsListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
