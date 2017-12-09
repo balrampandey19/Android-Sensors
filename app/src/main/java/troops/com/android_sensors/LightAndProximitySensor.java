f01dfe0a770e09b5c3949d64999b6c3540adaf0b
Balram Pandey <balrampandey19@gmail.com>
2017-12-09 23:56:37 +0530

    initial commit

--- app/src/main/java/troops/com/android_sensors/LightAndProximitySensor.java
+++ app/src/main/java/troops/com/android_sensors/LightAndProximitySensor.java
@@ -2,12 +2,16 @@ package troops.com.android_sensors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LightAndProximitySensor extends AppCompatActivity {
+    TextView light,proximity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_and_proximity_sensor);
+        light = (TextView)findViewById(R.id.sensor_light);
+
     }
 }
