8e28348a2cecdf6a177fca2b6b97a9c5064067ab
Balram Pandey <balrampandey19@gmail.com>
2017-12-11 19:03:07 +0530

    SensorEventListener

--- app/src/main/java/troops/com/android_sensors/SensorBasedOrientationGame.java
+++ app/src/main/java/troops/com/android_sensors/SensorBasedOrientationGame.java
@@ -1,13 +1,26 @@
 package troops.com.android_sensors;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SensorBasedOrientationGame extends AppCompatActivity {
+public class SensorBasedOrientationGame extends AppCompatActivity  implements SensorEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_based_orientation_game);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
