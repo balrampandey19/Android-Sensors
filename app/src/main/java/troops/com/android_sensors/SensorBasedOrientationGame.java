a935912d13e44c1794eb5d56d1335bc6fcbbfa39
Balram Pandey <balrampandey19@gmail.com>
2017-12-11 19:04:49 +0530

    Bind views

--- app/src/main/java/troops/com/android_sensors/SensorBasedOrientationGame.java
+++ app/src/main/java/troops/com/android_sensors/SensorBasedOrientationGame.java
@@ -3,15 +3,30 @@ package troops.com.android_sensors;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class SensorBasedOrientationGame extends AppCompatActivity  implements SensorEventListener {
+public class SensorBasedOrientationGame extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensorAccelerometer;
+    private Sensor mSensorMagnetometer;
+
+    private TextView mTextSensorAzimuth;
+    private TextView mTextSensorPitch;
+    private TextView mTextSensorRoll;
+    private static final float VALUE_DRIFT = 0.05f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_based_orientation_game);
+        mTextSensorAzimuth =  findViewById(R.id.value_azimuth);
+        mTextSensorPitch =  findViewById(R.id.value_pitch);
+        mTextSensorRoll =  findViewById(R.id.value_roll);
+
     }
 
     @Override
